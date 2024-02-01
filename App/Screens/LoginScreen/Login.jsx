364a5c554cfd20292a8a23efec3cc2963944311e
Drew Nelson <realseal177@gmail.com>
2024-02-01 15:18:06 -0800

    Utils and Colors.js added

--- App/Screens/LoginScreen/Login.jsx
+++ App/Screens/LoginScreen/Login.jsx
@@ -1,5 +1,6 @@
 import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import Colors from '../../Utils/Colors'
 
 export default function Login() {
   return (
@@ -14,6 +15,9 @@ export default function Login() {
 const styles = StyleSheet.create({
     loginImage: {
         width: 230,
-        height: 450
+        height: 450,
+        marginTop: 70,
+        borderWidth: 4,
+        borderColor: Colors.BLACK
     }
 })

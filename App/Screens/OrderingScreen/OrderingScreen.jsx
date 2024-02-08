e732f8700d28d7b6d92f8c127aa2f30b3446f38b
Drew Nelson <realseal177@gmail.com>
2024-02-08 13:50:10 -0800

    ordering added

--- App/Screens/OrderingScreen/OrderingScreen.jsx
+++ App/Screens/OrderingScreen/OrderingScreen.jsx
@@ -1,10 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import Colors from '../../Utils/Colors'
 
 export default function OrderingScreen() {
   return (
-    <View>
-      <Text>OrderingScreen</Text>
+    <View style={styles.subContainer}>
+            <Text style={{fontSize: 27, color: Colors.BLUE, textAlign: 'center'}}>
+                <Text style={{fontWeight: 'bold'}}>Order Burgers Here</Text>
+            </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    subContainer: {
+        width: '100%',
+        backgroundColor: Colors.SECONDARY,
+        height: '100%',
+        marginTop: '-20',
+        borderTopRightRadius: 30,
+        borderTopLeftRadius: 30,
+        padding: 20
+    }
+})
--- App/Utils/Colors.js
+++ App/Utils/Colors.js
@@ -1,5 +1,7 @@
 export default{
     BLACK: '#000',
     PRIMARY: '#8E3FFF',
-    WHITE: '#fff'
+    SECONDARY: '#ffb703',
+    WHITE: '#fff',
+    BLUE: '#3a86ff'
 }

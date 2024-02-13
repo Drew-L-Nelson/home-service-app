dd6ffb65aca92ff5592f97c38c5322f4aa9d0161
Drew Nelson <realseal177@gmail.com>
2024-02-13 11:44:04 -0800

    profileMainContainer added, styled

--- App/Screens/HomeScreen/Header.jsx
+++ App/Screens/HomeScreen/Header.jsx
@@ -2,19 +2,23 @@ import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { useUser } from '@clerk/clerk-expo'
 import Colors from '../../Utils/Colors';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Header() {
     const {user, isLoading} = useUser();
 
     return user&&(
         <View style={styles.container}>
-            <View>
-                <Image source={{ uri: user?.imageUrl }}
-                style={styles.userImage} />
-                <View>
-                    <Text style={{color: Colors.WHITE}}>Welcome, </Text>
-                    <Text style={{color: Colors.WHITE}}>{user.firstName} 🙂</Text>
+            <View style={styles.profileMainContainer}>
+                <View style={styles.profileContainer}>
+                    <Image source={{ uri: user?.imageUrl }}
+                    style={styles.userImage} />
+                    <View>
+                        <Text style={{color: Colors.WHITE}}>Welcome, </Text>
+                        <Text style={{color: Colors.WHITE, fontSize: 20}}>{user.firstName} 🙂</Text>
+                    </View>
                 </View>
+                <Ionicons name="bookmark-outline" size={24} color="white" />
             </View>
         </View>
     )
@@ -27,6 +31,18 @@ const styles = StyleSheet.create({
         backgroundColor: Colors.ORANGE2,
         borderBottomLeftRadius: 25,
         borderBottomRightRadius: 25
+    },
+    profileMainContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    profileContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10
     },
     userImage: {
         width: 45,

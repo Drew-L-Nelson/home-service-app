5a5e42112cc28cd849ecd5e5716229aad24ecff4
Drew Nelson <realseal177@gmail.com>
2024-02-13 11:23:21 -0800

    more colors added, container styles added

--- App/Screens/HomeScreen/Header.jsx
+++ App/Screens/HomeScreen/Header.jsx
@@ -1,21 +1,36 @@
 import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { useUser } from '@clerk/clerk-expo'
+import Colors from '../../Utils/Colors';
 
 export default function Header() {
     const {user, isLoading} = useUser();
 
-    return (
-        <View>
-            <Image source={{ uri: user?.imageUrl }}
-            style={styles.userImage} />
+    return user&&(
+        <View style={styles.container}>
+            <View>
+                <Image source={{ uri: user?.imageUrl }}
+                style={styles.userImage} />
+                <View>
+                    <Text style={{color: Colors.WHITE}}>Welcome, </Text>
+                    <Text style={{color: Colors.WHITE}}>{user.firstName} 🙂</Text>
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        paddingTop: 40,
+        backgroundColor: Colors.ORANGE2,
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25
+    },
     userImage: {
         width: 45,
-        height: 45
+        height: 45,
+        borderRadius: 99
     }
 })

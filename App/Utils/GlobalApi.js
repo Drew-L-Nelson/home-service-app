1c7b76dac982d99ca53621d02589e3b2116bbc0e
Drew Nelson <realseal177@gmail.com>
2024-02-15 12:11:56 -0800

    GlobaApi created using graphql-request

---
+++ App/Utils/GlobalApi.js
@@ -0,0 +1,22 @@
+import { request, gql } from 'graphql-request';
+
+const MASTER_URL = 'https://api-us-west-2.hygraph.com/v2/clsnlp0j10vz201uphr3kwc7c/master';
+
+const getSlider = async() => {
+    const query = gql`
+    query GetSlider {
+        sliders {
+          id
+          name
+          image {
+            url
+          }
+        }
+      }
+`
+
+const result = await request(MASTER_URL, query);
+    return result;
+}
+
+export default getSlider;

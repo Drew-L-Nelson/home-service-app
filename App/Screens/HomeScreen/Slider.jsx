3f84e3bd3e718f330cc2a5eacca8ccff8ce32df9
Drew Nelson <realseal177@gmail.com>
2024-02-17 12:56:03 -0800

    sliders fetching

--- App/Screens/HomeScreen/Slider.jsx
+++ App/Screens/HomeScreen/Slider.jsx
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import GlobalApi from '../../Utils/GlobalApi';
 
 export default function Slider() {
 
+    const [slider, setSlider] = useState();
+
     const getSliders = () => {
         GlobalApi.getSlider().then(resp => {
-            console.log('resp', resp);
+            console.log('resp', resp.sliders);
+            setSlider(resp?.sliders);
         })
     }
 

ea9b70d3c8af1a737473f2829398a1d607afa4e4
Drew Nelson <realseal177@gmail.com>
2024-02-24 11:13:48 -0800

    HomeNavigation now in place for HomeScreen

--- App/Navigations/HomeNavigation.jsx
+++ App/Navigations/HomeNavigation.jsx
@@ -1,13 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../Screens/HomeScreen/HomeScreen'
+import BusinessListByCategory from '../Screens/BusinessListByCategory/BusinessListByCategory';
+
+import { createStackNavigator } from '@react-navigation/stack'
 
 const Stack = createStackNavigator();
 
 export default function HomeNavigation() {
   return (
-    <Stack.Navigator>
-        <Stack.Screen />
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name='home' component={HomeScreen} />
+        <Stack.Screen name='business-list' component={BusinessListByCategory} />
     </Stack.Navigator>
   )
 }

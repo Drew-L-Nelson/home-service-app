4de30c75ba7477a858e86d6ce22687eef271d897
Drew Nelson <realseal177@gmail.com>
2024-02-27 09:45:56 -0800

    address fixed in schemas. Now rendering correctly

--- App/Screens/BusinessListByCategory/BusinessListItem.jsx
+++ App/Screens/BusinessListByCategory/BusinessListItem.jsx
@@ -1,17 +1,29 @@
 import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import Colors from '../../Utils/Colors'
 
 export default function BusinessListItem({business}) {
   return (
-    <View>
+    <View style={styles.container}>
       <Image source={{uri:business?.images[0]?.url}} 
         style={styles.image}
       />
+      <View>
+        <Text>{business.contactPerson}</Text>
+        <Text>{business.name}</Text>
+        <Text>{business.address1}</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create ({
+    container: {
+        padding: 10,
+        backgroundColor: Colors.WHITE,
+        borderRadius: 15,
+        marginBottom: 15
+    },
     image: {
         width: 100,
         height: 100,

fd740b9bc6f88d1225727057338409c70272fc19
Drew Nelson <realseal177@gmail.com>
2024-02-27 20:59:00 -0800

    BusinessDetailsScreen added

--- App/Navigations/HomeNavigation.jsx
+++ App/Navigations/HomeNavigation.jsx
@@ -2,8 +2,9 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import HomeScreen from '../Screens/HomeScreen/HomeScreen'
 import BusinessListByCategory from '../Screens/BusinessListByCategory/BusinessListByCategory';
+import BusinessDetailScreen from '../Screens/BusinessDetailsScreen/BusinessDetailScreen';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import { createStackNavigator } from '@react-navigation/stack'
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ export default function HomeNavigation() {
     <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name='home-screen' component={HomeScreen} />
         <Stack.Screen name='business-list' component={BusinessListByCategory} />
+        <Stack.Screen name='business-detail' component={BusinessDetailScreen} />
     </Stack.Navigator>
   )
 }

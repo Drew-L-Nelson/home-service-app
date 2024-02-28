89f9bac8f0de8b06df4e732b0f286b05641f69fc
Drew Nelson <realseal177@gmail.com>
2024-02-28 09:30:14 -0800

    back button added, opacity problem solved

--- App/Screens/BusinessDetailsScreen/BusinessDetailsScreen.jsx
+++ App/Screens/BusinessDetailsScreen/BusinessDetailsScreen.jsx
@@ -1,18 +1,34 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
+import { Ionicons } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native'
+import Colors from '../../Utils/Colors';
 
 export default function BusinessDetailsScreen() {
 
   const param = useRoute().params;
+  const navigation = useNavigation();
   const [business, setBusiness] = useState(param.business);
 
   return (
     <View>
-      <Text>Hello</Text>
       <Image source={{uri:business?.images[0]?.url}} 
         style={{width:'100%',height:300}}
       />
+      <TouchableOpacity onPress={()=>navigation.goBack()} style={styles.backBtn}>
+        <Ionicons name="arrow-back-outline" size={30} color="black" />
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create ({
+  backBtn: {
+    position: 'absolute',
+    zIndex: 10,
+    padding: 17,
+    margin: 2,
+    borderRadius: 99,
+    backgroundColor: 'rgba(128, 128, 128, 0.6)'
+  }
+})

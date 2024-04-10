f77c61aa12f31456f5b78299610c76cb15f5228f
Drew Nelson <realseal177@gmail.com>
2024-04-10 10:36:51 -0700

    getFirebaseStories working

--- App/Utils/FirebaseModules/APIs/getStories.jsx
+++ App/Utils/FirebaseModules/APIs/getStories.jsx
@@ -2,13 +2,11 @@ import { collection, getDocs } from "firebase/firestore";
 import { ref, getDownloadURL } from "firebase/storage";
 import { db, storage } from "../../firebase-config";
 
-const storiesCollectionRef = collection(db, 'Stories');
-
 // Manual test with a hardcoded image path
 const testRef = ref(storage, 'Story/coaxialglacier1984_Burger_fries_and_shake_brand_packaging_for_L_17cdf8fd-9665-4609-a81b-b42d9585b510.webp');
 getDownloadURL(testRef)
     .then(url => {
-        console.log('Test image URL:', url);
+        // console.log('Test image URL:', url);
     })
     .catch(error => {
         console.error('Error fetching test image:', error);
@@ -16,11 +14,14 @@ getDownloadURL(testRef)
 
 
 const getFirebaseStories = async () => {
+    const storiesCollectionRef = collection(db, 'Stories');
     const storyDocs = await getDocs(storiesCollectionRef);
     const stories = await Promise.all(storyDocs.docs.map(async doc => {
         const data = doc.data();
-        const imageRef = ref(storage, `Story/${data}`)
+        // console.log('doc.data() -->', data)
+        const imageRef = ref(storage, 'Story/coaxialglacier1984_Burger_fries_and_shake_brand_packaging_for_L_17cdf8fd-9665-4609-a81b-b42d9585b510.webp')
         const imageUrl = await getDownloadURL(imageRef);
+        // console.log('imageUrl -->', imageUrl)
         return {
             id: doc.id,
             name: data.name,
